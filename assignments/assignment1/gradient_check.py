2412e57c72199c47c1d0d10e023a75fea6f70861
CuSynth <k.korzhanevskii@g.nsu.ru>
2022-02-26 22:22:27 +0700

    Task 1.2 solved

--- assignments/assignment1/gradient_check.py
+++ assignments/assignment1/gradient_check.py
@@ -31,16 +31,13 @@ def check_gradient(f, x, delta=1e-5, tol = 1e-4):
     while not it.finished:
         ix = it.multi_index
         analytic_grad_at_ix = analytic_grad[ix]
-        x_at_ix = x[ix]
+                
+        orig_x[ix] += delta
+        fx_l = f(orig_x)[0]
         
-        x[ix] = x_at_ix + delta
-        fx2 = f(x)[0]
-        
-        x[ix] = x_at_ix - delta
-        fx1 = f(x)[0]
-        
-        x[ix] = x_at_ix
-        numeric_grad_at_ix = (fx2 - fx1) / (2 * delta)
+        orig_x[ix] -= 2*delta
+        fx_r = f(orig_x)[0]
+        numeric_grad_at_ix = (fx_l - fx_r) / (2*delta)
         
         if not np.isclose(numeric_grad_at_ix, analytic_grad_at_ix, tol):
             print("Gradients are different at %s. Analytic: %2.5f, Numeric: %2.5f" % (ix, analytic_grad_at_ix, numeric_grad_at_ix))
